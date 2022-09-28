21c0cc356c2e06f7404c9734649cbb27f3953edf
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-09-28 18:31:44 +0200

    Enabling HTTPS.

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -1,19 +1,28 @@
 import express from "express";
 import path from "path"; //path is a native Node package.
-import http from "http"; //Me too.
+import https from "https"; //Me too.
+import fs from "fs"; //file system
 
 const app: express.Application = express(); //O ur app helps us set up our server.
 const port = process.env.PORT || 8080; //'process' has info about the current process and we can get an environment variables.
 const router = express.Router();
+
+/* Note that we need to intall 'mkcert' and generate certificates 
+in order for the https to work during developent. */
+const options = {
+  key: fs.readFileSync("./cert/localhost-key.pem"),
+  cert: fs.readFileSync("./cert/localhost.pem"),
+};
+
 app.use(express.json()); //Middleware are functions that will be run prior to getting to our routes.
-app.use(express.static(path.join(__dirname, "../dist"))); //Where to find the statically served content. 
+app.use(express.static(path.join(__dirname, "../dist"))); //Where to find the statically served content.
 
 app.get("/", (req: express.Request, res: express.Response) => {
   const htmlFile = path.join(__dirname, "../dist/index.html");
   res.status(200).send(htmlFile);
 });
 
-const server = http.createServer({}, app);
+const server = https.createServer(options, app);
 
 //Callback to m
 server.listen(port, () => {
