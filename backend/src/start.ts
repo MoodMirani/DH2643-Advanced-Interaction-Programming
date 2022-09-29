14a0481e7941c83b5c34cca55f4a1359715c53ef
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-09-29 16:01:01 +0200

    Preparting for cookies as at the end of lecture 3.

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -3,6 +3,7 @@ import path from "path"; //path is a native Node package.
 import https from "https"; //Me too.
 import fs from "fs"; //file system
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app: express.Application = express(); //Our 'app' helps us set up our server.
 const port = process.env.PORT || 8080; //'process' has info about the current process and we can get an environment variables.
@@ -11,9 +12,28 @@ const router = express.Router();
 /* Example of how separation between production and development environment 
    might be achieved. */
 if (!(process.env.NODE_ENV === "production")) {
-  //Development environment can be configured here...
+  //Development environment can be configured here.
+
+  //Resolves CORS issues during development.
   app.use(cors({ credentials: true, origin: `https://localhost:8080/` }));
 }
+
+app.use(cookieParser());
+app.use(express.json()); //Middleware are functions that will be run prior to getting to our routes.
+app.use(express.static(path.join(__dirname, "../dist"))); //Where to find the statically served content.
+/*  To use authentication on the main route, we'd do something like */
+app.use(
+  "/",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    /* To access and validate tokens sent by the client, we can typically do 
+       something like this: */
+    //       const auth = req.headers.;
+    //       if(!auth) res.status(403).send(some object with other information)
+    /* Find the token, validate it with some library, then if it's valid, continue
+       with calling next() function in the middleware chain. */
+    next();
+  }
+);
 
 /* Note that we need to brew install 'mkcert' and generate certificates 
 in order for the https to work during developent. */
@@ -22,12 +42,20 @@ const options = {
   cert: fs.readFileSync("./cert/localhost.pem"),
 };
 
-app.use(express.json()); //Middleware are functions that will be run prior to getting to our routes.
-app.use(express.static(path.join(__dirname, "../dist"))); //Where to find the statically served content.
-
 app.get("/", (req: express.Request, res: express.Response) => {
   const htmlFile = path.join(__dirname, "../dist/index.html");
-  res.status(200).send(htmlFile);
+  res
+    .status(200)
+    // If we're using our server for authorization, we may put out token into
+    // the second parameter of cookie(), that is 'Some enctypted cookie value'.
+    .cookie(`My Cookie`, "Some enctypted cookie value", {
+      //maxAge: 5000
+      expires: new Date("2023-08-09"),
+      secure: true, //Makes cookie inaccessible through document.cookie.
+      httpOnly: true, //Actually https only.
+      sameSite: "lax", //Related to CORS, makes sure it's only used in our use case.
+    })
+    .send(htmlFile);
 });
 
 router.get(
@@ -40,11 +68,11 @@ router.get(
     }, 1000);
   }
 );
-//Now try setting up some code on the client to fetch this.
 
+//Now try setting up some code on the client to fetch this.
 const server = https.createServer(options, app);
 
-//Callback to m
+//Callback
 server.listen(port, () => {
   console.log(`Server is listening on port ${port}.`);
 });
