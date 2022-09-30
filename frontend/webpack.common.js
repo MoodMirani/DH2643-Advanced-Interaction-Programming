3bf5d2891dc9888dd61257470f2a76f5533f6926
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-09-30 14:09:04 +0200

    Lecture 1-3 and tsconfig.json problem

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,21 +1,30 @@
-import path from 'path';
+import path from "path";
 
 const loaders = [];
 loaders.push({
-    test: /\.jsx?$/,
-    exclude: '/node_modules',
-    use: 'babel-loader'
-})
+  test: /\.jsx?$/,
+  exclude: "/node_modules",
+  use: "babel-loader",
+});
+
+loaders.push({
+  test: /\.(ts|tsx)$/,
+  use: "ts-loader",
+  exclude: /node_modules/,
+});
 
 export default {
-    entry: {
-        app: './app.jsx'
-    }, 
-    module: {
-        rules: loaders
-    },
-    output: {
-        filename: 'main.bundle.js',
-        path: path.resolve(path.resolve(), "dist")
-    }
-}
+  entry: {
+    app: "./src/app.tsx",
+  },
+  module: {
+    rules: loaders,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  output: {
+    filename: "main.bundle.js",
+    path: path.resolve(path.resolve(), "../dist"),
+  },
+};
