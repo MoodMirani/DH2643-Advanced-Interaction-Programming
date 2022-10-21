e41495151263c878113d2c345dd40c4049cb547a
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-10-21 03:43:19 +0200

    Able to register and log in as a registered user via /api/auth/register and https://localhost:8080/api/auth/login respectively.

--- backend/src/auth/Auth.ts
+++ backend/src/auth/Auth.ts
@@ -22,3 +22,28 @@ export async function register(
       .send({ message: "User not created.", error: error.message });
   }
 }
+
+export async function login(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Password or username missing." });
+  }
+  try {
+    const user = await UserModel.findOne({ username, password });
+    if (!user) {
+      res
+        .status(401)
+        .json({ message: "Unable to login.", error: "No user found." });
+    } else {
+      res.status(200).json({ message: "Logged in.", user });
+    }
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error logging in.", error: error.message });
+  }
+}
