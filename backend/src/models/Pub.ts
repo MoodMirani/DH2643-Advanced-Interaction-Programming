02179233d4d8ff9478c4c565ec8ba79c93c289f7
Erik Martines Sanches <erik.martinessanches@gmail.com>
2022-10-23 23:27:26 +0200

    Endpoint for inserting a pub visit as a logged in user

---
+++ backend/src/models/Pub.ts
@@ -0,0 +1,19 @@
+// import { Schema, model } from "mongoose";
+// import { PatchSchema, PatchModel, Patch } from "./Patch";
+
+// export interface Pub {
+//   pub_name: string;
+//   opening_hours: string;
+//   description: string;
+
+//   patches: [PatchSchema];
+// }
+
+// const schema = new Schema<Pub>({
+//   pub_name: { type: String, unique: false, required: true },
+//   opening_hours: { type: String, unique: false, required: false },
+//   description: { type: String, unique: false, required: false },
+//   patches: [PatchSchema],
+// });
+// export const PubModel = model<Pub>("Pub", schema);
+//export default UserModel;
